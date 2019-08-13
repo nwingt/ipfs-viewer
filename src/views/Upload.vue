25c0a3ee3ec7417fac3f1d9c4459a157f0f90d40
Ng Wing Tat, David <i@ngwingt.at>
2019-08-13 19:03:14 +0800

    💄 Add logo in home page

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="about">
     <v-container class="blue-grey lighten-4" fluid>
-      <v-container>
-        <div class="display-2 mt-12 mb-2 font-weight-bold blue-grey--text text--darken-3">ISCN</div>
+      <v-container class="mt-12">
+        <v-img src="../assets/logo.svg" max-width="120" />
+        <div class="display-2 my-2 font-weight-bold blue-grey--text text--darken-3">ISCN</div>
         <h3 class="title blue-grey--text mb-8">
           International Standard Content Number, a worldwide unique number and metadata to identify
           a content
