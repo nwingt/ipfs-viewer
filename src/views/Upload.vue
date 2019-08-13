731dbe12d3152ac7f98f4cbaa89ff13a1e14fe13
Ng Wing Tat, David <i@ngwingt.at>
2019-08-13 16:31:58 +0800

    ✨ Add ISCN introduction

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,7 +1,18 @@
 <template>
-  <v-container class="about">
-    <UploadForm />
-  </v-container>
+  <div class="about">
+    <v-container class="blue-grey lighten-4" fluid>
+      <v-container>
+        <div class="display-2 mt-12 mb-2 font-weight-bold blue-grey--text text--darken-3">ISCN</div>
+        <h3 class="title blue-grey--text mb-8">
+          International Standard Content Number, a worldwide unique number and metadata to identify
+          a content
+        </h3>
+      </v-container>
+    </v-container>
+    <v-container fluid>
+      <UploadForm />
+    </v-container>
+  </div>
 </template>
 
 <script>
